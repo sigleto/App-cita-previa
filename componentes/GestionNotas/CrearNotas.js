e8c1db1577a00418e160968b5b73b6b0522ea193
sigleto <sigleto@gmail.com>
2025-02-06 05:35:03 +0100

    nueva nota

--- componentes/GestionNotas/CrearNotas.js
+++ componentes/GestionNotas/CrearNotas.js
@@ -7,9 +7,13 @@ import {
   TouchableOpacity,
   Alert,
   StyleSheet,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
 } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import * as Notifications from "expo-notifications";
+import DateTimePicker from "@react-native-community/datetimepicker";
 import { useNavigation } from "@react-navigation/native";
 import { Picker } from "@react-native-picker/picker";
 import {
@@ -24,6 +28,9 @@ export default function CrearNota() {
   const [noteTitle, setNoteTitle] = useState("");
   const [noteText, setNoteText] = useState("");
   const [categoria, setCategoria] = useState("Personal");
+  const [showDatePicker, setShowDatePicker] = useState(false);
+  const [showTimePicker, setShowTimePicker] = useState(false);
+  const [date, setDate] = useState(new Date());
   const navigation = useNavigation();
   const adUnitId = __DEV__
     ? TestIds.ADAPTIVE_BANNER
@@ -46,6 +53,7 @@ export default function CrearNota() {
         texto: noteText,
         categoria,
         favorito: false,
+        recordatorio: categoria === "Recordatorio" ? date.toISOString() : null,
       };
 
       const storedNotes = await AsyncStorage.getItem("notas");
@@ -57,21 +65,24 @@ export default function CrearNota() {
       setNoteTitle("");
       setNoteText("");
       setCategoria("Personal");
-      scheduleNotification(note.titulo);
+      if (categoria === "Recordatorio") {
+        scheduleNotification(note.titulo, date);
+      }
     } catch (error) {
       console.error("Error al guardar la nota:", error);
     }
   };
 
-  const scheduleNotification = async (noteTitle) => {
+  const scheduleNotification = async (noteTitle, noteDate) => {
     await Notifications.scheduleNotificationAsync({
       content: {
         title: "Recordatorio de Nota",
         body: `No olvides: ${noteTitle}`,
       },
-      trigger: { seconds: 5 },
+      trigger: { date: noteDate },
     });
   };
+
   const shareApp = async () => {
     try {
       const result = await Share.share({
@@ -92,8 +103,25 @@ export default function CrearNota() {
       console.error("Error al compartir:", error);
     }
   };
+
+  const handleDateChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShowDatePicker(false);
+    setDate(currentDate);
+    setShowTimePicker(true);
+  };
+
+  const handleTimeChange = (event, selectedTime) => {
+    const currentTime = selectedTime || date;
+    setShowTimePicker(false);
+    setDate(currentTime);
+  };
+
   return (
-    <View style={styles.container}>
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
       <TouchableOpacity onPress={shareApp} style={styles.shareIcon}>
         <MaterialCommunityIcons
           name="share-variant"
@@ -101,52 +129,79 @@ export default function CrearNota() {
           color="#007BFF"
         />
       </TouchableOpacity>
-      <Text style={styles.header}>Nueva Nota</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Título de la nota"
-        value={noteTitle}
-        onChangeText={setNoteTitle}
-      />
-      <TextInput
-        style={[styles.input, styles.textArea]}
-        placeholder="Escribe tu nota aquí..."
-        value={noteText}
-        onChangeText={setNoteText}
-        multiline
-      />
-
-      <Text style={styles.label}>Categoría:</Text>
-      <Picker
-        selectedValue={categoria}
-        onValueChange={setCategoria}
-        style={styles.picker}
-      >
-        <Picker.Item label="Personal" value="Personal" />
-        <Picker.Item label="Trabajo" value="Trabajo" />
-        <Picker.Item label="Recordatorio" value="Recordatorio" />
-      </Picker>
-
-      <TouchableOpacity style={styles.button} onPress={saveNote}>
-        <Icon name="save-outline" size={24} color="#fff" />
-        <Text style={styles.buttonText}>Guardar Nota</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity
-        style={[styles.button, styles.secondaryButton]}
-        onPress={() => navigation.navigate("ConsultarNotas")}
-      >
-        <Icon name="book-outline" size={24} color="#fff" />
-        <Text style={styles.buttonText}>Ver Notas Guardadas</Text>
-      </TouchableOpacity>
-
-      <View style={styles.banner}>
-        <BannerAd
-          unitId={adUnitId}
-          size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER}
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <Text style={styles.header}>Nueva Nota</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Título de la nota"
+          value={noteTitle}
+          onChangeText={setNoteTitle}
         />
-      </View>
-    </View>
+        <TextInput
+          style={[styles.input, styles.textArea]}
+          placeholder="Escribe tu nota aquí..."
+          value={noteText}
+          onChangeText={setNoteText}
+          multiline
+        />
+
+        <Text style={styles.label}>Categoría:</Text>
+        <Picker
+          selectedValue={categoria}
+          onValueChange={(itemValue) => {
+            setCategoria(itemValue);
+            if (itemValue === "Recordatorio") {
+              setShowDatePicker(true);
+            } else {
+              setShowDatePicker(false);
+            }
+          }}
+          style={styles.picker}
+        >
+          <Picker.Item label="Personal" value="Personal" />
+          <Picker.Item label="Trabajo" value="Trabajo" />
+          <Picker.Item label="Recordatorio" value="Recordatorio" />
+        </Picker>
+
+        {showDatePicker && (
+          <DateTimePicker
+            value={date}
+            mode="date"
+            display="default"
+            onChange={handleDateChange}
+          />
+        )}
+
+        {showTimePicker && (
+          <DateTimePicker
+            value={date}
+            mode="time"
+            display="default"
+            onChange={handleTimeChange}
+          />
+        )}
+
+        <TouchableOpacity style={styles.button} onPress={saveNote}>
+          <Icon name="save-outline" size={24} color="#fff" />
+          <Text style={styles.buttonText}>Guardar Nota</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={[styles.button, styles.secondaryButton]}
+          onPress={() => navigation.navigate("ConsultarNotas")}
+        >
+          <Icon name="book-outline" size={24} color="#fff" />
+          <Text style={styles.buttonText}>Ver Notas Guardadas</Text>
+        </TouchableOpacity>
+
+        <View style={styles.banner}>
+          <BannerAd
+            unitId={adUnitId}
+            size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER}
+          />
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
 
@@ -155,6 +210,10 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 20,
     backgroundColor: "#fdf7e1",
+    justifyContent: "center",
+  },
+  scrollContainer: {
+    flexGrow: 1,
     justifyContent: "center",
   },
   header: {
@@ -209,9 +268,11 @@ const styles = StyleSheet.create({
   },
   shareIcon: {
     position: "absolute",
-    top: 90,
+    top: 50,
     right: 10,
     zIndex: 1,
     backgroundColor: "#edf7d9",
+    padding: 5,
+    borderRadius: 20,
   },
 });
